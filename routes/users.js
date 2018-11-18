98d08634eb11bdecacb98d5eca3a12afffd84e4b
avistheripper <bboyxns@mail.ru>
2018-11-18 14:28:25 -0500

    Post request handle + server validation + bcrypt added

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const usersRouter = express.Router();
+/*
+@GET '/login' render login form
+@GET '/register' render register form
+@POST 
+*/
 
 usersRouter.get('/login', (req, res) => {
     res.render('users/login');
@@ -8,6 +13,22 @@ usersRouter.get('/login', (req, res) => {
 
 usersRouter.get('/register', (req, res) => {
     res.render('users/register')
+});
+
+usersRouter.post('/register', ({body}, res) => {
+    const {name, email, password, password2} = body;
+    let errors = [];
+    if(password != password2) errors.push({text: "Passwords doesn't match"});
+    if(password.length < 10) errors.push({text: 'The password is too short'});
+    if(errors.length > 1) {
+        res.render('users/register', ({
+            errors,
+            name,
+            email
+        }));
+    } else {
+        res.send('passed');
+    }
 });
 
 module.exports = usersRouter;
